7a5d36e600ed4017fe99251f2b178b92d032cd6e
Parth Kothari <112945837+Parth-Kothari-09@users.noreply.github.com>
2025-09-03 04:31:04 +0530

    Update HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,5 +1,5 @@
 public class HelloJenkins {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI in Java!");
+        System.out.println("Hello from Jenkins CI v2 in Java!");
     }
 }
